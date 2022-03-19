e62c473a875695d8ebc6899fa5c94c3629109c05
jbo4vv <84517851+jbo4vv@users.noreply.github.com>
2022-03-19 13:37:31 -0400

    tiktok daily assignment progress

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Video from "./Video.js";
 
 function App() {
   return (
     <div className="app">
       <h1>Hello World</h1>
+
+    <div className="app__videos">
+      <Video />
+      <Video />
+    </div>
+
 
       {/*app container */}
         {/* videos */}
---
+++ tiktok/src/Video.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Video.css";
+
+function Video() {
+    return <div className="video">
+
+        <video 
+        className="video__player"
+        loop
+        src ="https://v16m-webapp.tiktokcdn-us.com/9a7186c249809a00f718040a601eeb8a/6236630a/video/tos/useast2a/tos-useast2a-ve-0068c001/96cbfc82e16948f0a92bea0d75b7ff7f/?a=1988&br=6556&bt=3278&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7Thuuu_DXq&l=2022031917081401011313502427E63E3D&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3h1Nmc6Zng8OTMzNzczM0ApZWQ5ZjU5PGQ1N2U7ZGRpOmdrMXJjcjRvaTBgLS1kMTZzc141Nl8yX2I0MzMzXzRfMmM6Yw%3D%3D&vl=&vr="></video>
+
+        {/*VideoFooter*/}
+        {/*VideoSidebar*/}
+
+    </div>;
+}
+
+export default Video
