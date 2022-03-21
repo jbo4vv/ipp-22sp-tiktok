8d7c7d284886f3fd5a6743fde67eee7626c81643
jbo4vv <84517851+jbo4vv@users.noreply.github.com>
2022-03-21 13:04:18 -0400

    tiktok daily updates

--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import "./Video.css";
 
 function Video() {
+    const [playing, setPlaying] = useState(false);
+
+
+    const videoRef =  useRef(null); 
+
+    
+    const handleVideoPress = () =>{
+        //if video playing --> pause
+        if (playing) {
+           videoRef.current.pause(); 
+           setPlaying(false);   
+        }
+         //otherwise --> play
+        else{
+            videoRef.current.play();
+            setPlaying(true);  
+        }
+    }
+
     return <div className="video">
 
         <video 
+        onClick= {handleVideoPress}
         className="video__player"
         loop
-        src ="https://v16m-webapp.tiktokcdn-us.com/a4014263750d0fd736401e7da1652a95/62379a5e/video/tos/useast5/tos-useast5-pve-0068-tx/2686fa15cdc14f79ae3c1408533c8eba/?a=1988&br=2832&bt=1416&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThK0r_DXq&l=2022032015191701011300624404C52A5C&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M25uMzU6ZjhyOzMzZzczNEApZjQ1NWQ5OTw6N2dmaDo5M2cxM2NwcjRfb2tgLS1kMS9zc2MxNi9eYl9hNTQuX2ItMDM6Yw%3D%3D&vl=&vr="></video>
+        ref={videoRef}
+        src ="https://v16m-webapp.tiktokcdn-us.com/17f30dbc6962735038da1794d3a862bf/62390085/video/tos/useast5/tos-useast5-ve-0068c004-tx/a60e8b1d9b134c00b2cb58d58563df2b/?a=1988&br=3516&bt=1758&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThogjMDXq&l=2022032116471901011313511722B528DE&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzZsZTQ6ZnhlOjMzZzczNEApOTMzaGY2N2Q7N2Y4NDVnOGdiY2FgcjRvZ2xgLS1kMS9zc2BfYzRgYF5hMi1jNS41MS06Yw%3D%3D&vl=&vr="></video>
 
         {/*VideoFooter*/}
         {/*VideoSidebar*/}
