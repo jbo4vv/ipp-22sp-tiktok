410b8f17e7b8969a80ff9d576429dc18429d08fd
jbo4vv <84517851+jbo4vv@users.noreply.github.com>
2022-03-24 12:00:50 -0400

    BIG tiktok changes

--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import "./Video.css";
 import VideoFooter from './VideoFooter';
+import VideoSidebar from './VideoSidebar';
 
 function Video() {
     const [playing, setPlaying] = useState(false);
@@ -29,13 +30,11 @@ function Video() {
         className="video__player"
         loop
         ref={videoRef}
-        src ="https://v16m-webapp.tiktokcdn-us.com/cae1e741fb6858e972b04651a93381df/623a524a/video/tos/alisg/tos-alisg-pve-0037c001/1ebab719f42647b185c0293fc97dd2c3/?a=1988&br=3588&bt=1794&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThRuzMDXq&l=202203221648320101131350430B84A2B9&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3A2Mzo6Zmk3OzMzODczNEApOTpmNjM2aWRlN2YzMzs1Z2dfL2xjcjRfX2VgLS1kMS1zcy0yYmBfYDNjLjBeMmNiLS46Yw%3D%3D&vl=&vr="></video>
+        src ="https://v16m-webapp.tiktokcdn-us.com/d769a17d34d382d3bc2d91a3462fe030/623ce0f9/video/tos/useast5/tos-useast5-ve-0068c004-tx/fa534fab842f4dc094531fa00328aa6b/?a=1988&br=5944&bt=2972&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7Th1p.JDXq&l=2022032415214701011313511005CA3805&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anA0ZGc6Zmc6OzMzZzczNEApZmY5ZThoOzw7NzU3aWZmNWcybHFvcjQwb3JgLS1kMS9zc181MV9eNi8vLzQ0YmNiMF46Yw%3D%3D&vl=&vr="></video>
 
 
         <VideoFooter />
-        
-        {/*VideoFooter*/}
-        {/*VideoSidebar*/}
+        <VideoSidebar />
 
     </div>;
 }
--- tiktok/src/VideoSidebar.js
+++ tiktok/src/VideoSidebar.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './VideoSidebar.css';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MessageIcon from '@mui/icons-material/Message';
+import ShareIcon from '@mui/icons-material/Share';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 function VideoSidebar() {
+
+  const [liked, setLiked] = useState(false);
+
   return (
     <div className="videoSidebar">
+      <div className="videoSidebar__button">
+        {liked ? (
+          <FavoriteIcon fontSize='large'/>
+        ) : (
+          <FavoriteBorderIcon
+          fontSize="large"onClick={(e) => setLiked(true)} />
+        )}
+        <p>100</p>
+      </div>
+      <div className="videoSidebar__button">
+        <MessageIcon fontSize ='large' />
+        <p>250</p>
+      </div>  
+      <div className="videoSidebar__button">
+        <ShareIcon fontSize = 'large' />;
+        <p>11</p>
+      </div>
 
 
     </div>
