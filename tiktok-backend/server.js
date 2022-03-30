a981cce2bc680b117007b50336ba5f83d7deb3e9
jbo4vv <84517851+jbo4vv@users.noreply.github.com>
2022-03-30 13:43:58 -0400

    daily updates

--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+//app config
 const app = express();
 const port = 9000;
 
+//middlewares
+
+//DB config
+
+//api endpoints
 app.get("/", (req,res) => res.status(200).send('Hello World'));
 
+//listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`))
 
