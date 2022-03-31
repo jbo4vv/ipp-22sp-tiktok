131af3aae394d0792d4ece72484c6371c53b089f
jbo4vv <84517851+jbo4vv@users.noreply.github.com>
2022-03-31 13:31:57 -0400

    tiktok daily progress

--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+
+
 //app config
 const app = express();
 const port = 9000;
@@ -8,6 +10,14 @@ const port = 9000;
 //middlewares
 
 //DB config
+const connection_url='mongodb+srv://admin:MN1y7ADGomSXJYTF@cluster0.54rpi.mongodb.net/tiktok?retryWrites=true&w=majority';
+
+mongoose.connect(connection_url,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
 
 //api endpoints
 app.get("/", (req,res) => res.status(200).send('Hello World'));
