ca98c1521adba1a4db2147d28940ea3683aaa40a
fariha-tasneem <98547769+fariha-tasneem@users.noreply.github.com>
2022-04-06 00:42:55 -0400

    Week 2 TikTok

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -8,8 +8,25 @@ function App() {
       
 
     <div className="app_videos">
-      <Video />
-      <Video />
+      <Video url="https://v16m-webapp.tiktokcdn-us.com/b0760988891c505ec69edb7e7be2da66/624d35fb/video/tos/useast5/tos-useast5-ve-0068c003-tx/4cfb588ba9ae403eb3567776e4bcb4e7/?a=1988&br=1968&bt=984&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7Th1DBSDXq&l=2022040600404301011300623322C053CF&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=
+      0&rc=ank2cTM6Zm9vPDMzZzczNEApZzk5aTMzNjtnNzw4Njk1aGc1ajMtcjRvXzJgLS1kMS9zc14tLzEuNF4vLl5iXi0tYzI6Yw%3D%3D&vl=&vr="
+      channel="some channel"
+      description="some description"
+      song="some song"
+      likes={123}
+      messages={456}
+      shares={789}
+      />
+      
+      <Video url="https://v16m-webapp.tiktokcdn-us.com/b0760988891c505ec69edb7e7be2da66/624d35fb/video/tos/useast5/tos-useast5-ve-0068c003-tx/4cfb588ba9ae403eb3567776e4bcb4e7/?a=1988&br=1968&bt=984&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7Th1DBSDXq&l=2022040600404301011300623322C053CF&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=
+      0&rc=ank2cTM6Zm9vPDMzZzczNEApZzk5aTMzNjtnNzw4Njk1aGc1ajMtcjRvXzJgLS1kMS9zc14tLzEuNF4vLl5iXi0tYzI6Yw%3D%3D&vl=&vr="
+      channel="some channel"
+      description="some description"
+      song="some song"
+      likes={123}
+      messages={456}
+      shares={789}
+      />
     </div>
 
       {/*app container */}
--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from 'react';
 import "./Video.css";
 import VideoFooter from './VideoFooter';
+import VideoSidebar from './VideoSidebar';
 
-function Video() {
+function Video( {url, channel, description, song, likes, messages, shares}) {
 
     const [playing, setPlaying] = useState(false);
 
@@ -28,11 +29,12 @@ function Video() {
         className="video_play"
         loop
         ref = {videoRef}
-        src ="https://v16m-webapp.tiktokcdn-us.com/1b642cad5c17c6acba85cc1e0351b4cb/6240f950/video/tos/useast5/tos-useast5-ve-0068c004-tx/ceafb01ba2594fd6a2bb969117d354f0/?a=1988&br=2114&bt=1057&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThmOjgDXq&l=202203271754420101131351170683552E&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2Y3d2k6ZnJnOzMzZzczNEApZmk0Mzw1OGU2NzpmOzZkNmdrXzRycjRvMG5gLS1kMS9zczIwNTRgYi1iLTExLi0xXjQ6Yw%3D%3D&vl=&vr="></video>
+        src ={url}></video>
 
-        <VideoFooter />
-        {/*VideoFooter*/}
-        {/*VideoSidebar*/}
+        <VideoFooter channel={'dogChannel'} 
+        description={'Dog'} song={'What a Wonderful World'}/>
+        <VideoSidebar likes ={likes} messages={messages}
+        shares = {shares} />
             
     </div>;
     
