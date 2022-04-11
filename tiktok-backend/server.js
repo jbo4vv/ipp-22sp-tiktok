9b326b0e6d3f4a81cf81cfa0029856d47653472b
jbo4vv <84517851+jbo4vv@users.noreply.github.com>
2022-04-11 18:10:46 -0400

    big up

--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -4,15 +4,16 @@ import data from "./data.js";
 import Videos from "./dbModel.js";
 
 
+
 //app config
 const app = express();
-const port = 9000;
+const port = process.env.PORT||9000;
 
 //middlewares
 app.use(express.json());
 app.use((req, res, next)=>{
-    res.setHeaders('Access-Control-Allow-Origin', '*'),
-    res.setHeaders('Access-Control-Allow-Headers', '*'),
+    res.setHeader('Access-Control-Allow-Origin', '*'),
+    res.setHeader('Access-Control-Allow-Headers', '*'),
     next()
 })
 
@@ -33,7 +34,7 @@ app.get("/", (req,res) => res.status(200).send('Hello World'));
 app.get('/v1/posts', (req,res) => res.status(200).send(data));
 
 app.get("/v2/posts", (req, res) => {
-    Videos.find((err, data) => {
+    Videos.find({},(err, data) => {
         if (err){
             res.status(500).send(err);
         }else{
