9fe7c08b99f38e38217da3d6b6b84af15408778e
Peter Kim <66498084+RavenousRave@users.noreply.github.com>
2022-04-12 21:32:51 -0400

    TikTok Week3

---
+++ tiktok-backend/data.js
@@ -0,0 +1,22 @@
+const data = [
+   {
+      url: "https://v16m-webapp.tiktokcdn-us.com/018fd9b238cfb273ef157af1edb84bc5/6256743b/video/tos/alisg/tos-alisg-pve-0037c001/118373847e6840fca533e16ab0404fb9/?a=1988&br=2192&bt=1096&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThcwSaDXq&l=202204130056430101131351432072AF16&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anc3dGU6ZjV1PDMzODczNEApNmU7ZmQ4Zjs7N2hpaWdlO2dwMzBvcjRnbzBgLS1kMS1zczQ1Ly82Y2I2X2JiMWIyYzI6Yw%3D%3D&vl=&vr=",
+      channel: "some channel",
+      description: "some description",
+      song: "some song",
+      likes: 123,
+      messages: 456,
+      shares: 789,
+   },
+   {
+      url: "https://v16m-webapp.tiktokcdn-us.com/018fd9b238cfb273ef157af1edb84bc5/6256743b/video/tos/alisg/tos-alisg-pve-0037c001/118373847e6840fca533e16ab0404fb9/?a=1988&br=2192&bt=1096&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThcwSaDXq&l=202204130056430101131351432072AF16&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anc3dGU6ZjV1PDMzODczNEApNmU7ZmQ4Zjs7N2hpaWdlO2dwMzBvcjRnbzBgLS1kMS1zczQ1Ly82Y2I2X2JiMWIyYzI6Yw%3D%3D&vl=&vr=",
+      channel: "some channel",
+      description: "some description",
+      song: "some song",
+      likes: 123,
+      messages: 456,
+      shares: 789,
+   }
+]
+
+export default data;
--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,9 +1,42 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Data from './data.js';
+import Videos from './dbModel.js';
 
+
+// app config
 const app = express();
 const port = 9000;
 
+// middlewares
+app.use(express.json());
+app.use((req,res,next) => {
+   res.setHeaders('Access-Control-Allow-Origin', '*'),
+   res.setHeaders('Access-Control-Allow-Headers', '*'),
+   next()
+})
+
+
+// DB Config
+const connection_url = 'mongodb+srv://admin:4wsjV9Cg72vnPiro@cluster0.avxqy.mongodb.net/tiktok?retryWrites=true&w=majority';
+mongoose.connect(connection_url, {
+   useNewUrlParser: true,
+   // userCreateIndex: true,
+   useUnifiedTopology: true,
+});
+//api endpoints
 app.get('/', (req,res) => res.status(200).send('Hello World'));
+
+app.get('/v1/posts', (req,res) => res.status(200).send(Data));
+
+app.post('/v2/posts', (req,res) => {
+   Videos.find( (err, data) => {
+      if (err) {
+         res.status(500).send(err);
+      } else {
+         res.status(201).send(data);
+      }
+   });
+})
 
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
