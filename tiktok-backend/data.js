5b9615d8e63043c557c51b5e8d44a864d18cc1e5
joykim1005 <81274936+joykim1005@users.noreply.github.com>
2022-04-13 15:47:25 -0400

    my first commit

---
+++ tiktok-backend/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url: "https://v16m-webapp.tiktokcdn-us.com/d9e1e72253f693f512c4a489d3464446/62576bd2/video/tos/useast2a/tos-useast2a-pve-0068/034839afab7c4f718415fe9be76ad585/?a=1988&br=2360&bt=1180&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=ebtHKH-qMyq8Zc8Bhwe2NVZefl7Gb&l=202204131833110101130060721C246D9A&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amRlOW5nZDlzdDMzNzczM0ApNjZpOGVkZ2UzNzlmOzo3ZGdmLTZsbXBmbWlfLS0xMTZzc2MxNmBhMV5fYGJgYWItNl86Yw%3D%3D&vl=&vr=",
+        channel: "someDummyChannel",
+        song: "someDummySong",
+        likes: "123",
+        messages: "456",
+        description: "someDummtDescription",
+        shares: "789",
+    },
+    {
+        url: "https://v16m-webapp.tiktokcdn-us.com/d9e1e72253f693f512c4a489d3464446/62576bd2/video/tos/useast2a/tos-useast2a-pve-0068/034839afab7c4f718415fe9be76ad585/?a=1988&br=2360&bt=1180&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=ebtHKH-qMyq8Zc8Bhwe2NVZefl7Gb&l=202204131833110101130060721C246D9A&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amRlOW5nZDlzdDMzNzczM0ApNjZpOGVkZ2UzNzlmOzo3ZGdmLTZsbXBmbWlfLS0xMTZzc2MxNmBhMV5fYGJgYWItNl86Yw%3D%3D&vl=&vr=",
+        channel: "someDummyChannel",
+        song: "someDummySong",
+        likes: "123",
+        messages: "456",
+        description: "someDummtDescription",
+        shares: "789",
+    }
+]
+
+export default data;
--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,9 +1,40 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Videos from './dbModel.js';
+import Data from "./data.js";
 
+const connection_url = "mongodb+srv://admin:MnNO1Fm4ubKV3tfy@cluster0.5ljzs.mongodb.net/tiktok?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url,{
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+//app config
 const app = express();
 const port = 9000;
 
+//middlewares
+app.use(express.json());
+//DB config
+
+//api endpoints
 app.get('/', (req,res) => res.status(200).send('Hello World'));
 
+app.get('/v1/posts', (req,res)=>res.status(200).send(Data));
+
+app.post('/v2/posts', (req,res)=>{
+    const dbVideos = req.body
+    Videos.create(dbVideos, (err, data) => {
+        if (err){
+            res.status(500).send(err);
+        } else{
+            res.status(201).send(data);
+        }
+    });
+})
+
+
+//listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
