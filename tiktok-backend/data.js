9fe7d2b47f6eb283d767622aae842bcf8a157e41
jbo4vv <84517851+jbo4vv@users.noreply.github.com>
2022-04-20 14:59:53 -0400

    debugging

--- tiktok-backend/data.js
+++ tiktok-backend/data.js
@@ -1,6 +1,6 @@
 const data = [
    {
-      url: "https://v16m-webapp.tiktokcdn-us.com/018fd9b238cfb273ef157af1edb84bc5/6256743b/video/tos/alisg/tos-alisg-pve-0037c001/118373847e6840fca533e16ab0404fb9/?a=1988&br=2192&bt=1096&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThcwSaDXq&l=202204130056430101131351432072AF16&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anc3dGU6ZjV1PDMzODczNEApNmU7ZmQ4Zjs7N2hpaWdlO2dwMzBvcjRnbzBgLS1kMS1zczQ1Ly82Y2I2X2JiMWIyYzI6Yw%3D%3D&vl=&vr=",
+      url: "https://v16m-webapp.tiktokcdn-us.com/c2f1f0b47724adf1a8e2ef694acc1076/6260abe3/video/tos/useast5/tos-useast5-ve-0068c002-tx/4fc841e70264495f903bfb5cc891f436/?a=1988&br=1680&bt=840&cd=0%7C0%7C0%7C0&ch=0&cr=0&cs=0&dr=0&ds=2&er=&ft=ebtHKH-qMyq8Z_8CNwe2N.-7fl7Gb&l=2022042018564001011300619417BE8C3F&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ampyNGU6ZjM0PDMzZzczNEApOmlpZzg7OWQ5NzpmO2Y2NWdrcnFocjRfNmRgLS1kMS9zcy5eM19gYmBgLTVeLmJjM186Yw%3D%3D&vl=&vr=",
       channel: "some channel",
       description: "some description",
       song: "some song",
--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,42 +1,63 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import Data from './data.js';
-import Videos from './dbModel.js';
+import data from "./data.js";
+import Videos from "./dbModel.js";
 
 
-// app config
+
+//app config
 const app = express();
-const port = 9000;
+const port = process.env.PORT||9000;
 
-// middlewares
+//middlewares
 app.use(express.json());
-app.use((req,res,next) => {
-   res.setHeaders('Access-Control-Allow-Origin', '*'),
-   res.setHeaders('Access-Control-Allow-Headers', '*'),
-   next()
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*'),
+    res.setHeader('Access-Control-Allow-Headers', '*'),
+    next()
 })
 
+//DB config
+//p-word:MN1y7ADGomSXJYTF
+const connection_url='mongodb+srv://admin:4wsjV9Cg72vnPiro@cluster0.avxqy.mongodb.net/tiktok?retryWrites=true&w=majority';
 
-// DB Config
-const connection_url = 'mongodb+srv://admin:4wsjV9Cg72vnPiro@cluster0.avxqy.mongodb.net/tiktok?retryWrites=true&w=majority';
-mongoose.connect(connection_url, {
-   useNewUrlParser: true,
-   // userCreateIndex: true,
-   useUnifiedTopology: true,
+mongoose.connect(connection_url,{
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true,
 });
+
+
 //api endpoints
-app.get('/', (req,res) => res.status(200).send('Hello World'));
+app.get("/", (req,res) => res.status(200).send('Hello World'));
 
-app.get('/v1/posts', (req,res) => res.status(200).send(Data));
+app.get('/v1/posts', (req,res) => res.status(200).send(data));
 
-app.post('/v2/posts', (req,res) => {
-   Videos.find( (err, data) => {
-      if (err) {
-         res.status(500).send(err);
-      } else {
-         res.status(201).send(data);
-      }
-   });
+app.get("/v2/posts", (req, res) => {
+    Videos.find({},(err, data) => {
+        if (err){
+            res.status(500).send(err);
+        }else{
+            res.status(200).send(data);
+        } 
+    }
+    )
 })
 
-app.listen(port, () => console.log(`listening on localhost: ${port}`));
+app.post('/v2/posts', (req,res) =>{
+    const dbVideos = req.body
+
+    Videos.create(dbVideos, (err, data) => {
+        if (err){
+            //500 = 'server error'
+            res.status(500).send(err);
+        }else{
+            //201 = 'created'
+            res.status(201).send(data);
+        }
+    });
+
+})
+
+//listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`))
